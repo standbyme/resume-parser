9082b2f682ba02636c479ff9fc653790f7b19beb
StandbyMe <stand-by-me@mail.com>
2018-04-13 11:17:12 +0800

    Final

--- src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
+++ src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
@@ -2,13 +2,26 @@ package StandbyMe.iPIN.ResumeParser
 
 import StandbyMe.XMLUtil.XMLSplit
 
-import scala.xml.XML
+import scala.xml.{Node, XML}
 
 object Hello extends App {
   val root = XML.loadFile("output.xml")
   val strategy = strategy_factory(root)
 
   val a = XMLSplit(strategy)((root \\ "_").toList)
-  println(a.pair__list.length)
-  a.pair__list.foreach(n => println(extract_simple_text_from_node(n._1)))
+  //  println(a.pair__list.length)
+
+  def pn(n: Node) = extract_simple_text_from_node(n)
+
+  def p(n: (Node, List[Node])) = {
+    val title = pn(n._1)
+    println(title)
+    val node__list = n._2
+    node__list.foreach(m => println(pn(m)))
+    println("------")
+  }
+
+  a.init.foreach(n => println(pn(n)))
+  println("//////")
+  a.pair__list.foreach(p)
 }
--- src/main/scala/StandbyMe/iPIN/ResumeParser/package.scala
+++ src/main/scala/StandbyMe/iPIN/ResumeParser/package.scala
@@ -14,13 +14,15 @@ package object ResumeParser {
 
   def strategy_factory(root_node: Node): (Node => Boolean) = {
     val key_font_size = get_font_sizes_of_node(root_node).toStream.distinct.sorted.reverse.apply(1)
-    println(key_font_size)
+    //    println(key_font_size)
 
     def strategy(node: Node): Boolean = {
-      val length_result = extract_simple_text_from_node(node).lengthCompare(1) > 0
+      val text = extract_simple_text_from_node(node)
+      val text_result = !text.contains("/")
+      val length_result = text.lengthCompare(1) > 0
       val font_sizes_of_node = get_font_sizes_of_node(node)
       val font_size_result = font_sizes_of_node.forall(_ == key_font_size) && (font_sizes_of_node.length > 0)
-      length_result && font_size_result
+      length_result && font_size_result && text_result
     }
 
     strategy

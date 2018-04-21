846c45065c4f6635d031b60f4e74a91e813864c0
StandbyMe <stand-by-me@mail.com>
2018-04-21 21:46:56 +0800

    Final

--- src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
+++ src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
@@ -1,6 +1,6 @@
 package StandbyMe.iPIN.ResumeParser
 
-import StandbyMe.XMLUtil.XMLSplit
+import StandbyMe.XMLUtil.{XMLSplit, XMLSplitResult}
 
 import scala.xml.{Node, XML}
 
@@ -9,19 +9,20 @@ object Hello extends App {
   val strategy = strategy_factory(root)
 
   val a = XMLSplit(strategy)((root \\ "_").toList)
-  //  println(a.pair__list.length)
+  val b = XMLSplitResult(a.init, a.pair__list.map { case (n, nl) => (n, nl.filter(extract_simple_text_from_node(_).lengthCompare(0) > 0)) })
 
   def pn(n: Node) = extract_simple_text_from_node(n)
 
   def p(n: (Node, List[Node])) = {
     val title = pn(n._1)
     println(title)
+    println("....")
     val node__list = n._2
     node__list.foreach(m => println(pn(m)))
     println("------")
   }
 
-  a.init.foreach(n => println(pn(n)))
+  b.init.foreach(n => println(pn(n)))
   println("//////")
-  a.pair__list.foreach(p)
+  b.pair__list.foreach(p)
 }

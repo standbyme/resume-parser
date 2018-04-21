5697e2fd701f899b26b72b5b7ea5e0dcafa15941
StandbyMe <stand-by-me@mail.com>
2018-04-22 07:19:02 +0800

    w:tc w:r

--- src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
+++ src/main/scala/StandbyMe/iPIN/ResumeParser/Hello.scala
@@ -2,7 +2,7 @@ package StandbyMe.iPIN.ResumeParser
 
 import StandbyMe.XMLUtil.{XMLSplit, XMLSplitResult}
 
-import scala.xml.{Node, XML}
+import scala.xml.{Node, NodeSeq, XML}
 
 object Hello extends App {
   val root = XML.loadFile("output.xml")
@@ -22,7 +22,28 @@ object Hello extends App {
     println("------")
   }
 
+  def condition_tbl(n: Node): Boolean = {
+    (n.label == "tc") && n.descendant.forall(_.label != "tc")
+  }
+
+  def condition_any(n: Node): Boolean = {
+    (n.label == "r") && n.descendant.forall(_.label != "r")
+  }
+
+  def k(n: Node): NodeSeq = {
+    val descendant = n \\ "_"
+    // if added to here
+    if (descendant.exists(_.label == "tbl")) descendant.filter(condition_tbl)
+    else descendant.filter(condition_any)
+  }
+
+  def f(n: (Node, List[Node])): (Node, List[Node]) = {
+    val (keynode, content_of_keynode) = n
+    val new_content_of_keynode = content_of_keynode.flatMap(k)
+    (keynode, new_content_of_keynode)
+  }
+
   b.init.foreach(n => println(pn(n)))
   println("//////")
-  b.pair__list.foreach(p)
+  b.pair__list.map(f).foreach(p)
 }
